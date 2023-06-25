ab7f500aaccaa3af753c61d5deca9d7a80fdc69d
senik <sebastienwenzel@gmail.com>
2023-06-26 10:10:31 +1100

    correction style

--- src/components/hero-project/Heroproject.js
+++ src/components/hero-project/Heroproject.js
@@ -1,3 +1,4 @@
+import PA from "./project/pa.png"
 import PB from "./project/pb.png"
 import PC from "./project/pc.png"
 import PD from "./project/pd.png"
@@ -12,69 +13,68 @@ const Heroproject = () => {
             <div className="heroproject_list">            
             <article className="heroproject_list_article">
                 <div className="heroproject_list_article--image">
-                    <a href="https://xsenikx.github.io/WenzelSebastien_ProjetOC_4_03-03-2022-optimiser/">
-                        <figure>
-                            <img src={PE} alt='Ohmyfood'/>
+                    <figure>
+                        <a href="https://xsenikx.github.io/WenzelSebastien_7_02_04_2023_kasa/">
+                            <img src={PA} alt='Kasa'/>
                             <div className="loupe">
                                 <i class="fa-solid fa-magnifying-glass"></i>
                             </div>
-                        </figure>
-                        
-                    </a>
+                        </a>
+                    </figure>  
                 </div>
                 <div className="heroproject_list_article--text">
-                    <p>Construisez un site e-commerce en JavaScript</p>
-                    <a href="https://github.com/xsenikx/WenzelSebastien_ProjetOC_5_29-06-2022"><i class="fa-brands fa-github"></i></a>
-                    <p>HTML - CSS - JavaScript</p>
+                    <p>Création une application web de location immobilière</p>
+                    <a href="https://github.com/xsenikx/WenzelSebastien_7_02_04_2023_kasa"><i class="fa-brands fa-github"></i></a>
+                    <p>HTML  CSS  JavaScript</p>
                 </div>
             </article>
             <article className="heroproject_list_article">
                 <div className="heroproject_list_article--image">
-                    <a href="https://xsenikx.github.io/WenzelSebastien_ProjetOC_4_03-03-2022-optimiser/">
-                        <figure>
+                    <figure>
+                        <a href="https://xsenikx.github.io/WenzelSebastien_ProjetOC_4_03-03-2022-optimiser/">
+
                             <img src={PE} alt='Ohmyfood'/>
                             <div className="loupe">
                                 <i class="fa-solid fa-magnifying-glass"></i>
                             </div>
-                        </figure>
-                        
-                    </a>
+                        </a>
+                    </figure>    
                 </div>
                 <div className="heroproject_list_article--text">
-                    <p>Construisez un site e-commerce en JavaScript</p>
-                    <a href="https://github.com/xsenikx/WenzelSebastien_ProjetOC_5_29-06-2022"><i class="fa-brands fa-github"></i></a>
-                    <p>HTML - CSS - JavaScript</p>
+                    <p> API sécurisée pour une application d'avis gastronomiques</p>
+                    <a href="https://github.com/xsenikx/WenzelSebastien_22_02_2023"><i class="fa-brands fa-github"></i></a>
+                    <p>NodeJs Express MongoDB</p>
                 </div>
             </article>
             <article className="heroproject_list_article">
                 <div className="heroproject_list_article--image">
-                    <a href="https://xsenikx.github.io/WenzelSebastien_ProjetOC_4_03-03-2022-optimiser/">
-                        <figure>
+                    <figure>
+                        <a href="https://xsenikx.github.io/WenzelSebastien_ProjetOC_4_03-03-2022-optimiser/">
+
                             <img src={PE} alt='Ohmyfood'/>
                             <div className="loupe">
                                 <i class="fa-solid fa-magnifying-glass"></i>
                             </div>
-                        </figure>
-                        
-                    </a>
+                        </a>
+                    </figure>        
                 </div>
                 <div className="heroproject_list_article--text">
-                    <p>Construisez un site e-commerce en JavaScript</p>
+                    <p> un site e-commerce en JavaScript</p>
                     <a href="https://github.com/xsenikx/WenzelSebastien_ProjetOC_5_29-06-2022"><i class="fa-brands fa-github"></i></a>
-                    <p>HTML - CSS - JavaScript</p>
+                    <p>HTML  CSS  JavaScript</p>
                 </div>
             </article>
             <article className="heroproject_list_article">
                 <div className="heroproject_list_article--image">
-                    <a href="https://xsenikx.github.io/WenzelSebastien_ProjetOC_4_03-03-2022-optimiser/">
-                        <figure>
+                    <figure>
+                        <a href="https://xsenikx.github.io/WenzelSebastien_ProjetOC_4_03-03-2022-optimiser/">
+
                             <img src={PD} alt='Ohmyfood'/>
                             <div className="loupe">
                                 <i class="fa-solid fa-magnifying-glass"></i>
                             </div>
-                        </figure>
-                        
-                    </a>
+                        </a>
+                    </figure>    
                 </div>
                 <div className="heroproject_list_article--text">
                     <p>Optimisez un site web existant</p>
@@ -84,38 +84,37 @@ const Heroproject = () => {
             </article>
             <article className="heroproject_list_article">
                 <div className="heroproject_list_article--image">
-                    <a href="https://xsenikx.github.io/WenzelSebastien_ProjetOC_3_11032021/">
-                        <figure>
+                    <figure>
+                        <a href="https://xsenikx.github.io/WenzelSebastien_ProjetOC_3_11032021/">
+
                             <img src={PC} alt='Ohmyfood'/>
                             <div className="loupe">
                                 <i class="fa-solid fa-magnifying-glass"></i>
                             </div>
-                        </figure>
-                        
-                    </a>
+                        </a>
+                    </figure>    
                 </div>
                 <div className="heroproject_list_article--text">
                     <p>Dynamisez une page web avec des animation</p>
                     <a href="https://github.com/xsenikx/WenzelSebastien_ProjetOC_3_11032021"><i class="fa-brands fa-github"></i></a>
-                    <p>HTML - CSS <br/> SASS</p>
+                    <p>HTML  CSS  SASS</p>
                 </div>
             </article>
             <article className="heroproject_list_article">
                 <div className="heroproject_list_article--image">
-                    <a href="https://xsenikx.github.io/WenzelSebastien_ProjetOC_2_12122020/">
-                        <figure>
+                    <figure>
+                        <a href="https://xsenikx.github.io/WenzelSebastien_ProjetOC_2_12122020/">
                             <img src={PB} alt='Ohmyfood'/>
                             <div className="loupe">
                                 <i class="fa-solid fa-magnifying-glass"></i>
                             </div>
-                        </figure>
-                        
-                    </a>
+                            </a>
+                    </figure>   
                 </div>
                 <div className="heroproject_list_article--text">
                     <p>Transformez une maquette en site web avec HTML & CSS</p>
                     <a href="https://github.com/xsenikx/WenzelSebastien_ProjetOC_2_12122020"><i class="fa-brands fa-github"></i></a>
-                    <p>HTML - CSS</p>
+                    <p>HTML  CSS</p>
                 </div>
             </article>
             </div>
